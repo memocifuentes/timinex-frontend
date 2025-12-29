dc4cefcfdad494ac776f042d9c3cf5f956685abc
Guillermo Cifuentes Román <guillermocifuentesroman@Guillermos-MBP.ht.home>
2025-12-29 11:47:00 -0500

    Minimal professional coming soon page

--- components/ui/input.tsx
+++
@@ -1,7 +0,0 @@
-"use client"
-
-import React from "react"
-
-export const Input = (props: React.InputHTMLAttributes<HTMLInputElement>) => (
-  <input {...props} className="border rounded px-2 py-1 w-full" />
-)
--- components/ui/textarea.tsx
+++
@@ -1,7 +0,0 @@
-"use client"
-
-import React from "react"
-
-export const Textarea = (props: React.TextareaHTMLAttributes<HTMLTextAreaElement>) => (
-  <textarea {...props} className="border rounded px-2 py-1 w-full" />
-)
